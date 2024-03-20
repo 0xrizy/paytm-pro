d957a0811667fb09d065faf69f7ef317e6575570
Rizul Thakur <81066183+0xrizy@users.noreply.github.com>
2024-03-20 18:53:11 +0530

    Update Send.jsx

--- frontend/src/Pages/Send.jsx
+++ frontend/src/Pages/Send.jsx
@@ -219,7 +219,7 @@ export default function Send() {
                 className="border border-gray-500 rounded-md p-1 mb-3"
               />
               <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-                {loading ? (
+                {!loading ? (
                   <button
                     onClick={sendTransaction}
                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
